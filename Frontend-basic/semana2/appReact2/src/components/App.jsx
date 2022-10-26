8b02848d586eaff7e1f10526421e1ff98a7484ba
Sergio-Ibarra-1795 <sergio.ibarra1795@gmail.com>
2022-10-25 20:24:17 -0500

    Explicación del hook useEffect y su uso en diferentes casos

--- Frontend-basic/semana2/appReact2/src/components/App.jsx
+++ Frontend-basic/semana2/appReact2/src/components/App.jsx
@@ -2,21 +2,38 @@ import { useEffect, useState } from 'react'
 import '../styles/App.css'
 
 function App () {
+  console.log('render')
 /* Reglas básicas de los hooks */
 /* 1. Debe ser llamado en la cabecera del componente */
 /* 2. No se pueden llamar dentro de condicionales, ciclos o scopes */
 
-  const [username, setUserName] = useState('Bruno')
-  const handleChangeName = () => setUserName('Jared')
+  /*const [username, setUserName] = useState('Bruno') */
+  const [number, setNumber] = useState(0)
+  /* const handleChangeName = () => setUserName('Jared') */
 
-  useEffect(() => {
+  /* useEffect SIEMPRE SE EJECUTA */
+  /*useEffect(() => {
     handleChangeName()
-  })
+    console.log('update')
+  }) */
+
+  /* versión de useEffect que se ejecuta una sola vez */
+/*
+  useEffect(()=>{
+    number == 0 && console.log('useEffect')
+  },[]) */
+
+  const productCount = 10
+
+  /* Version que se ejeucta cuando detecta el cambio en un estado específico  */
+  useEffect(() => {
+    number === productCount && window.alert('YA NO HAY PRODUCTOS')
+  }, [number])
 
   return (
     <div className='App'>
-      <h2>{username}</h2>
-      <button onClick={handleChangeName}>Cambiar</button>
+      <h2>{number}</h2>
+      <button onClick={() => number < productCount && setNumber(number + 1)}>Cambiar</button>
     </div>
   )
 }
