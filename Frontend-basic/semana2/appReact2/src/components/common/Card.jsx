0c00f57ffd3c6ce7d1eaf2b059e37a8a3f8c341f
Sergio-Ibarra-1795 <sergio.ibarra1795@gmail.com>
2022-10-31 20:02:57 -0600

    Creating dinamic URL and paths for the countries App in React

--- Frontend-basic/semana2/appReact2/src/components/common/Card.jsx
+++ Frontend-basic/semana2/appReact2/src/components/common/Card.jsx
@@ -1,15 +1,22 @@
+import { Link } from 'react-router-dom'
+import toLowerCase from '../../utils/toLowerCase'
+
 export default function Card ({ ...props }) {
-    const { flags, name } = props
-    return (
-      <div className='card'>
-        <img
-          src={flags.svg}
-          className='card-img-top'
-          alt={name.common}
-        />
-        <div className='card-body'>
-          <h5 className='card-title'>{name.common}</h5>
-        </div>
+  const { flags, name, cca3 } = props
+
+  const lowerCca3 = toLowerCase(cca3)
+
+  return (
+    <Link className='card' to={`/country/${lowerCca3}`}>
+      <img
+        src={flags.svg}
+        className='card-img-top'
+        alt={name.common}
+        lazy='loading'
+      />
+      <div className='card-body'>
+        <h5 className='card-title m-0'>{name.common}</h5>
       </div>
-    )
-  }
+    </Link>
+  )
+}
